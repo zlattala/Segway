bf3fcaca5e729ea7cc470241cb2b4fe11bd98103
zlattala <33968813+zlattala@users.noreply.github.com>
2023-08-21 13:29:23 +0100

    updated segway comparison with fixed balancestm_p2

--- RoboToolFiles/SegwayComparison/src-gen/SegwayController.h
+++ RoboToolFiles/SegwayComparison/src-gen/SegwayController.h
@@ -5,10 +5,9 @@
 #include "RoboCalcAPI/Controller.h"
 #include "DataTypes.h"
 
-#include "BalanceSTM.h"
-#include "AnglePID.h"
-#include "SpeedPID.h"
-#include "RotationPID.h"
+#include "BalanceSTM_P2.h"
+#include "SpeedPID_S.h"
+#include "RotationPID_S.h"
 
 class SegwayController: public robocalc::Controller 
 {
@@ -20,10 +19,9 @@ public:
 	
 	void Execute()
 	{
-		balanceSTM.execute();
-		anglePID.execute();
-		speedPID.execute();
-		rotationPID.execute();
+		balanceSTM_P2.execute();
+		speedPID_S.execute();
+		rotationPID_S.execute();
 	}
 	
 	struct Channels
@@ -33,117 +31,160 @@ public:
 		
 		EventBuffer* tryEmitLeftMotorVelocity(void* sender, std::tuple<double> args)
 		{
-			if(instance.balanceSTM.canReceiveLeftMotorVelocity(args))
-				instance.balanceSTM.leftMotorVelocity_in.trigger(sender, args);
+			if(instance.balanceSTM_P2.canReceiveLeftMotorVelocity(args))
+				instance.balanceSTM_P2.leftMotorVelocity_in.trigger(sender, args);
 				
-			return &instance.balanceSTM.leftMotorVelocity_in;
+			return &instance.balanceSTM_P2.leftMotorVelocity_in;
 		}
 		
 		EventBuffer* tryEmitRightMotorVelocity(void* sender, std::tuple<double> args)
 		{
-			if(instance.balanceSTM.canReceiveRightMotorVelocity(args))
-				instance.balanceSTM.rightMotorVelocity_in.trigger(sender, args);
+			if(instance.balanceSTM_P2.canReceiveRightMotorVelocity(args))
+				instance.balanceSTM_P2.rightMotorVelocity_in.trigger(sender, args);
 				
-			return &instance.balanceSTM.rightMotorVelocity_in;
+			return &instance.balanceSTM_P2.rightMotorVelocity_in;
 		}
 		
 		EventBuffer* tryEmitAngle(void* sender, std::tuple<double> args)
 		{
-			if(instance.balanceSTM.canReceiveAngle(args))
-				instance.balanceSTM.angle_in.trigger(sender, args);
+			if(instance.balanceSTM_P2.canReceiveAngle(args))
+				instance.balanceSTM_P2.angle_in.trigger(sender, args);
 				
-			return &instance.balanceSTM.angle_in;
+			return &instance.balanceSTM_P2.angle_in;
 		}
 		
 		EventBuffer* tryEmitGyroX(void* sender, std::tuple<double> args)
 		{
-			if(instance.balanceSTM.canReceiveGyroX(args))
-				instance.balanceSTM.gyroX_in.trigger(sender, args);
+			if(instance.balanceSTM_P2.canReceiveGyroX(args))
+				instance.balanceSTM_P2.gyroX_in.trigger(sender, args);
 				
-			return &instance.balanceSTM.gyroX_in;
+			return &instance.balanceSTM_P2.gyroX_in;
 		}
 		
 		EventBuffer* tryEmitGyroY(void* sender, std::tuple<double> args)
 		{
-			if(instance.balanceSTM.canReceiveGyroY(args))
-				instance.balanceSTM.gyroY_in.trigger(sender, args);
+			if(instance.balanceSTM_P2.canReceiveGyroY(args))
+				instance.balanceSTM_P2.gyroY_in.trigger(sender, args);
 				
-			return &instance.balanceSTM.gyroY_in;
+			return &instance.balanceSTM_P2.gyroY_in;
 		}
 		
 		EventBuffer* tryEmitGyroZ(void* sender, std::tuple<double> args)
 		{
-			if(instance.balanceSTM.canReceiveGyroZ(args))
-				instance.balanceSTM.gyroZ_in.trigger(sender, args);
+			if(instance.balanceSTM_P2.canReceiveGyroZ(args))
+				instance.balanceSTM_P2.gyroZ_in.trigger(sender, args);
 				
-			return &instance.balanceSTM.gyroZ_in;
-		}
-		
-		EventBuffer* tryEmitAngleOutputE(void* sender, std::tuple<double> args)
-		{
-			if(instance.balanceSTM.canReceiveAngleOutputE(args))
-				instance.balanceSTM.angleOutputE_in.trigger(sender, args);
-				
-			return &instance.balanceSTM.angleOutputE_in;
+			return &instance.balanceSTM_P2.gyroZ_in;
 		}
 		
 		EventBuffer* tryEmitAnewError(void* sender, std::tuple<double> args)
 		{
-			if(instance.anglePID.canReceiveAnewError(args))
-				instance.anglePID.anewError_in.trigger(sender, args);
-				
-			return &instance.anglePID.anewError_in;
+			anewError_in.trigger(sender, args);
+			return &anewError_in;
 		}
 		
 		EventBuffer* tryEmitAdiff(void* sender, std::tuple<double> args)
 		{
-			if(instance.anglePID.canReceiveAdiff(args))
-				instance.anglePID.adiff_in.trigger(sender, args);
+			adiff_in.trigger(sender, args);
+			return &adiff_in;
+		}
+		
+		EventBuffer* tryEmitAngleOutputE(void* sender, std::tuple<double> args)
+		{
+			if(instance.balanceSTM_P2.canReceiveAngleOutputE(args))
+				instance.balanceSTM_P2.angleOutputE_in.trigger(sender, args);
 				
-			return &instance.anglePID.adiff_in;
+			return &instance.balanceSTM_P2.angleOutputE_in;
 		}
 		
 		EventBuffer* tryEmitSpeedOutputE(void* sender, std::tuple<double> args)
 		{
-			if(instance.balanceSTM.canReceiveSpeedOutputE(args))
-				instance.balanceSTM.speedOutputE_in.trigger(sender, args);
+			if(instance.balanceSTM_P2.canReceiveSpeedOutputE(args))
+				instance.balanceSTM_P2.speedOutputE_in.trigger(sender, args);
 				
-			return &instance.balanceSTM.speedOutputE_in;
+			return &instance.balanceSTM_P2.speedOutputE_in;
 		}
 		
 		EventBuffer* tryEmitSnewError(void* sender, std::tuple<double> args)
 		{
-			if(instance.speedPID.canReceiveSnewError(args))
-				instance.speedPID.snewError_in.trigger(sender, args);
+			if(instance.speedPID_S.canReceiveSnewError(args))
+				instance.speedPID_S.snewError_in.trigger(sender, args);
 				
-			return &instance.speedPID.snewError_in;
+			return &instance.speedPID_S.snewError_in;
 		}
 		
 		EventBuffer* tryEmitRotationOutputE(void* sender, std::tuple<double> args)
 		{
-			if(instance.balanceSTM.canReceiveRotationOutputE(args))
-				instance.balanceSTM.rotationOutputE_in.trigger(sender, args);
+			if(instance.balanceSTM_P2.canReceiveRotationOutputE(args))
+				instance.balanceSTM_P2.rotationOutputE_in.trigger(sender, args);
 				
-			return &instance.balanceSTM.rotationOutputE_in;
+			return &instance.balanceSTM_P2.rotationOutputE_in;
 		}
 		
 		EventBuffer* tryEmitRdiff(void* sender, std::tuple<double> args)
 		{
-			if(instance.rotationPID.canReceiveRdiff(args))
-				instance.rotationPID.rdiff_in.trigger(sender, args);
+			if(instance.rotationPID_S.canReceiveRdiff(args))
+				instance.rotationPID_S.rdiff_in.trigger(sender, args);
 				
-			return &instance.rotationPID.rdiff_in;
+			return &instance.rotationPID_S.rdiff_in;
 		}
 		
+		struct AnewError_t : public EventBuffer
+		{
+			THREAD_SAFE_ONLY(std::mutex _mutex;)
+			std::tuple<double> _args;
+			void* _sender = nullptr;
+			void* getSender() override
+			{
+				THREAD_SAFE_ONLY(std::lock_guard<std::mutex> lock{_mutex};)
+				return _sender;
+			}
+			
+			void reset() override
+			{
+				THREAD_SAFE_ONLY(std::lock_guard<std::mutex> lock{_mutex};)
+				_sender = nullptr;
+			}
+			
+			void trigger(void* sender, std::tuple<double> args)
+			{
+				THREAD_SAFE_ONLY(std::lock_guard<std::mutex> lock{_mutex};)
+				_args = args;
+				_sender = sender;
+			}
+		} anewError_in;
+		struct Adiff_t : public EventBuffer
+		{
+			THREAD_SAFE_ONLY(std::mutex _mutex;)
+			std::tuple<double> _args;
+			void* _sender = nullptr;
+			void* getSender() override
+			{
+				THREAD_SAFE_ONLY(std::lock_guard<std::mutex> lock{_mutex};)
+				return _sender;
+			}
+			
+			void reset() override
+			{
+				THREAD_SAFE_ONLY(std::lock_guard<std::mutex> lock{_mutex};)
+				_sender = nullptr;
+			}
+			
+			void trigger(void* sender, std::tuple<double> args)
+			{
+				THREAD_SAFE_ONLY(std::lock_guard<std::mutex> lock{_mutex};)
+				_args = args;
+				_sender = sender;
+			}
+		} adiff_in;
 	};
 	
 	Channels channels{*this};
 	
 	SegwayRP* platform;
-	BalanceSTM_StateMachine<SegwayController> balanceSTM{*platform, *this, &balanceSTM};
-	AnglePID_StateMachine<SegwayController> anglePID{*platform, *this, &anglePID};
-	SpeedPID_StateMachine<SegwayController> speedPID{*platform, *this, &speedPID};
-	RotationPID_StateMachine<SegwayController> rotationPID{*platform, *this, &rotationPID};
+	BalanceSTM_P2_StateMachine<SegwayController> balanceSTM_P2{*platform, *this, &balanceSTM_P2};
+	SpeedPID_S_StateMachine<SegwayController> speedPID_S{*platform, *this, &speedPID_S};
+	RotationPID_S_StateMachine<SegwayController> rotationPID_S{*platform, *this, &rotationPID_S};
 };
 
 #endif
